8c1e9fd1bdf6d5d3b22674e1737fea28b18b8ef7
Thomas Pope <tpope0928@gmail.com>
2018-08-23 22:14:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  def completes(name_hash) # This also works for this lab.
-  name_hash.sort do |x,y|
-     x <=> y
-  end.first[0]
-end
+ 
 end
